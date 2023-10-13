c634b0ccab8b310ffe6e087fd5866b372fe804a8
hellolin-oi <hellolinoi@outlook.com>
2023-10-13 16:08:48 +0800

    feat: ✨ P9311 [EGOI2021] Twin Cookies / 姐妹分饼干

---
+++ Luogu/p9311.cpp
@@ -0,0 +1,121 @@
+/*
+ * |-| [- |_ |_ () |_ | |\| 's Competitive Code Template v3.4.2
+ * Recommended standard: C++20.
+ * In general, this source code can be found archived here:
+ *     https://github.com/hellolin-oi/CP-Code
+ */
+#ifndef ONLINE_JUDGE
+#pragma GCC optimize(2, "inline", "unroll-loops", "no-stack-protector", "fast-math")
+#endif
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,avx,avx2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+#include <emmintrin.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/priority_queue.hpp>
+#include <immintrin.h>
+// #include "atcoder/all"
+#if defined(_WIN64) || defined(__x86_64__) || __WORDSIZE == 64
+#define ARCH64
+#endif
+
+namespace hellolin {
+#define str std::string
+#define vec std::vector
+#define pr std::pair
+#define pq __gnu_pbds::priority_queue
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = short;
+using u16 = unsigned short;
+using i64 = long long;
+using u64 = unsigned long long;
+#ifdef ARCH64
+using i128 = __int128_t;
+using u128 = __uint128_t;
+#else
+#warning The int128 type has been fallbacked to int64.
+using i128 = i64;
+using u128 = u64;
+#endif
+using ld = long double;
+#define rdecltype(x) std::decay_t<decltype(x)>
+#define _rep1(x) for (rdecltype(x) i = 1, i##end = x; i <= i##end; ++i)
+#define _rep2(i, x) for (rdecltype(x) i = 1, i##end = x; i <= i##end; ++i)
+#define _rep3(i, x, y) for (auto i = x, i##end = y; i <= i##end; ++i)
+#define _rep4(i, x, y, z) for (auto i = x, i##end = y; i <= i##end; i += (z))
+#define _per1(x) for (rdecltype(x) i = x, i##end = 1; i >= i##end; --i)
+#define _per2(i, x) for (rdecltype(x) i = x, i##end = 1; i >= i##end; --i)
+#define _per3(i, x, y) for (auto i = x, i##end = y; i >= i##end; --i)
+#define _per4(i, x, y, z) for (auto i = x, i##end = y; i >= i##end; i -= (z))
+#define ovld(_1, _2, _3, _4, x, ...) x
+#define rep(...) ovld(__VA_ARGS__, _rep4, _rep3, _rep2, _rep1)(__VA_ARGS__)
+#define per(...) ovld(__VA_ARGS__, _per4, _per3, _per2, _per1)(__VA_ARGS__)
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define fi first
+#define se second
+#define ps emplace
+#define pb emplace_back
+constexpr char newl = '\n';
+template <class T1, class T2>
+inline bool chmax(T1 &x, T2 y) { return x < y ? x = y, 1 : 0; }
+template <class T1, class T2>
+inline bool chmin(T1 &x, T2 y) { return x > y ? x = y, 1 : 0; }
+constexpr int N = 5e3 + 11;
+constexpr i64 LIMIT = 2e15;
+int n;
+i64 r[N], t;
+std::mt19937_64 mt(std::chrono::steady_clock::now().time_since_epoch().count());
+std::uniform_int_distribution<i64> dist(1, LIMIT);
+#define rnd() dist(mt)
+std::set<i64> p, q;
+void solve() {
+    std::cin >> n;
+    rep(100) {
+        std::cout << '?';
+        rep(j, 1, n) {
+            auto x = rnd();
+            while (p.count(x))
+                x = rnd();
+            p.insert(x);
+            std::cout << ' ' << x;
+        }
+        std::cout << std::endl;
+        std::cin >> r[i];
+    }
+    rep(100) {
+        rep(j, i + 1, 100) {
+            rep(k, j + 1, 100) {
+                if ((int) q.size() == n) break;
+                auto x = r[i] + r[j] + r[k];
+                if (!q.count(x))
+                    q.insert(x);
+            }
+        }
+    }
+    std::cout << '?';
+    for (auto &i : q) {
+        std::cout << ' ' << i;
+    }
+    std::cout << std::endl;
+    std::cin >> t;
+    std::cout << "! 1 3" << std::endl;
+    std::cout << t << std::endl;
+    rep(100) {
+        rep(j, i + 1, 100) {
+            rep(k, j + 1, 100) {
+                if (r[i] + r[j] + r[k] == t) {
+                    std::cout << r[i] << ' ' << r[j] << ' ' << r[k] << std::endl;
+                    return;
+                }
+            }
+        }
+    }
+}
+} // namespace hellolin
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cin.exceptions(std::cin.failbit);
+    hellolin::solve();
+    return 0;
+}
