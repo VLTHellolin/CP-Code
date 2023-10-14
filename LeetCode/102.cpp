7f496f4f5c15bc074d98beb3dbaeaef388c3ec8e
hellolin-oi <hellolinoi@outlook.com>
2023-10-14 22:12:06 +0800

    feat: ✨ 102. 二叉树的层序遍历

---
+++ LeetCode/102.cpp
@@ -0,0 +1,73 @@
+// Hellolin's Code Template (LeetCode) v4.0.0
+// *INDENT-OFF*
+// NOLINTBEGIN
+// clang-format off
+#define HLCTv4 1
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/priority_queue.hpp>
+namespace hellolin {
+using namespace std;
+namespace def {
+using i8 = signed char; using u8 = unsigned char; using i16 = short; using u16 = unsigned short;
+using i64 = long long; using u64 = unsigned long long; using f32 = float; using f64 = long double; using f96 = long double;
+#ifdef __SIZEOF_INT128__
+using i128 = __int128_t; using u128 = __uint128_t;
+#else
+using i128 = i64; using u128 = u64;
+#endif
+#define ps emplace
+#define pb emplace_back
+#define newl ('\n')
+#define sorl(a, b) (" \n"[a==b])
+} using namespace def;
+namespace util {
+template<class T,class U>inline bool chmax(T&x,U y){return y>x?x=y,1:0;}
+template<class T,class U>inline bool chmin(T&x,U y){return y<x?x=y,1:0;}
+} using namespace util;
+// *INDENT-ON*
+// NOLINTEND
+// clang-format on
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr) {}
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x)
+        , left(left)
+        , right(right) {}
+};
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode *rt) {
+        vector<vector<int>> res;
+        queue<TreeNode *> q;
+        if (rt == nullptr) return res;
+        q.ps(rt);
+        while (q.size()) {
+            int cur = q.size();
+            res.pb(vector<int>{});
+            for (int i = 1; i <= cur; ++i) {
+                auto x = q.front();
+                res.back().pb(x->val);
+                if (x->left) q.ps(x->left);
+                if (x->right) q.ps(x->right);
+                q.pop();
+            }
+        }
+        return res;
+    }
+};
+
+} // namespace hellolin
+
+using namespace hellolin;
